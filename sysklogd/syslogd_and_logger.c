f3a7e659252cea3d9161d0845590034a8e78877a
haohaoget <2287405159wh@gmail.com>
2026-04-16 21:28:07 +0800

    feat: add syslogd_and_logger.c to provide missing SYSLOG_NAMES definitions for Android Bionic

--- sysklogd/syslogd_and_logger.c
+++ sysklogd/syslogd_and_logger.c
@@ -12,6 +12,37 @@
 #define SYSLOG_NAMES_CONST
 #include <syslog.h>
 
+#if defined(__ANDROID__)
+/* Android Bionic's unified sysroot lacks SYSLOG_NAMES */
+typedef struct _code {
+	char *c_name;
+	int c_val;
+} CODE;
+static const CODE prioritynames[] = {
+    { "alert", LOG_ALERT }, { "crit", LOG_CRIT }, { "debug", LOG_DEBUG },
+    { "emerg", LOG_EMERG }, { "err", LOG_ERR }, { "error", LOG_ERR },
+    { "info", LOG_INFO }, { "none", -1 }, { "notice", LOG_NOTICE },
+    { "panic", LOG_EMERG }, { "warn", LOG_WARNING }, { "warning", LOG_WARNING },
+    { NULL, -1 }
+};
+static const CODE facilitynames[] = {
+    { "auth", LOG_AUTH }, { "authpriv", LOG_AUTHPRIV }, { "cron", LOG_CRON },
+    { "daemon", LOG_DAEMON }, { "ftp", LOG_FTP }, { "kern", LOG_KERN },
+    { "lpr", LOG_LPR }, { "mail", LOG_MAIL }, { "mark", (24<<3) }, /* INTERNAL_MARK */
+    { "news", LOG_NEWS }, { "security", LOG_AUTH }, { "syslog", LOG_SYSLOG },
+    { "user", LOG_USER }, { "uucp", LOG_UUCP }, { "local0", LOG_LOCAL0 },
+    { "local1", LOG_LOCAL1 }, { "local2", LOG_LOCAL2 }, { "local3", LOG_LOCAL3 },
+    { "local4", LOG_LOCAL4 }, { "local5", LOG_LOCAL5 }, { "local6", LOG_LOCAL6 },
+    { "local7", LOG_LOCAL7 }, { NULL, -1 }
+};
+#ifndef INTERNAL_NOPRI
+#define INTERNAL_NOPRI 0x10
+#endif
+#ifndef INTERNAL_MARK
+#define INTERNAL_MARK (24<<3)
+#endif
+#endif
+
 #if 0
 /* For the record: with SYSLOG_NAMES <syslog.h> defines
  * (not declares) the following:
